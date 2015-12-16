a091c01ef97a0e250f777ac3250fd8da85ef20c3
Sameesh <sameesh.fisat@gmail.com>
2015-12-16 01:51:41 -0500

    Update Read.java

--- Read.java
+++ Read.java
@@ -7,11 +7,21 @@ public static void main(String[] args) throws IOException {
 
 
 Scanner scanner = new Scanner(new File("dat.txt"));
-int [] tall = new int [100];
-int i = 0;
-while(scanner.hasNextInt()){
-   tall[i++] = scanner.nextInt();
-   System.out.println(tall[i-1]);
+int [] X = new int [100];
+int [] Y =new int [100];
+int i = 0,count=1;
+	while(scanner.hasNextInt()){
+	if(i%2==0)
+		{
+		X[count] = scanner.nextInt();
+		System.out.println(X[count]+":"+count);
+		}
+	else
+		{
+		Y[count++] = scanner.nextInt();
+		System.out.println(Y[count]+":"+count);
+		}
+	i++;
 	}
 	}
 }
